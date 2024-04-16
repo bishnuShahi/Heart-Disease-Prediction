d9c3d2ce870477e98761a0a04a38d60bbb6aff33
Bishnu Shahi <113760159+bishnuShahi@users.noreply.github.com>
2024-04-16 20:38:36 +0530

    Update settings.py

--- Interface/settings.py
+++ Interface/settings.py
@@ -74,14 +74,10 @@ WSGI_APPLICATION = 'Interface.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-if not DEBUG:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }}
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
