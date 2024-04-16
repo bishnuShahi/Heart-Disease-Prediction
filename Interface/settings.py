18aa592ee0069b7f7d0cdc210cf095f4378a1c7f
Bishnu Shahi <113760159+bishnuShahi@users.noreply.github.com>
2024-04-16 20:19:42 +0530

    Update settings.py

--- Interface/settings.py
+++ Interface/settings.py
@@ -74,15 +74,14 @@ WSGI_APPLICATION = 'Interface.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
-        
-    }
-}
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 
 # Password validation
