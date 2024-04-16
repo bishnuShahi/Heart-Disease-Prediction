c9007c4a1cc48060077c46fb52a7fcb669bfb18d
Bishnu Shahi <113760159+bishnuShahi@users.noreply.github.com>
2024-04-16 20:26:37 +0530

    Update settings.py

--- Interface/settings.py
+++ Interface/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4&^d@03+pz$mqx*v4+3anoxd)c-)w*u&9lfv7w_y6z9t05io$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
