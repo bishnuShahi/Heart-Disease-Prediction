9c0367b36ef46006fbc1613b507d224c703b5f16
Bishnu Shahi <113760159+bishnuShahi@users.noreply.github.com>
2024-04-16 19:28:56 +0530

    Update settings.py

--- Interface/settings.py
+++ Interface/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
+        
     }
 }
 
