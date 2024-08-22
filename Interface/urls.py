fdb516c4d76abb942bfdd2eb9b239f47e8935a8e
Bishnu Shahi <113760159+bishnuShahi@users.noreply.github.com>
2024-08-22 16:46:12 +0530

    Update urls.py

--- Interface/urls.py
+++ Interface/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from . import views, settings
+from django.urls import path, include, re_path
+from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('predict/', views.predict, name='predict'),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
